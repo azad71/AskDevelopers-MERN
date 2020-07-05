055f0795be7f12f9b6ca00b1883611f0bd108b59
azad71 <mdmamun732@gmail.com>
2020-07-05 13:16:13 +0600

    redux devtools extension is removed

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,19 +1,21 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 
 import rootReducer from "./root-reducer";
 
 const middlewares = [thunk];
 
-let devTools = null;
+// let devTools = null;
 
-if (process.env.NODE_ENV !== "production") {
-  devTools =
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__();
-}
+// if (process.env.NODE_ENV !== "production") {
+//   devTools =
+//     window.__REDUX_DEVTOOLS_EXTENSION__ &&
+//     window.__REDUX_DEVTOOLS_EXTENSION__();
+// }
 
-export const store = createStore(
-  rootReducer,
-  compose(applyMiddleware(...middlewares), devTools)
-);
+// export const store = createStore(
+//   rootReducer,
+//   compose(applyMiddleware(...middlewares), devTools)
+// );
+
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
